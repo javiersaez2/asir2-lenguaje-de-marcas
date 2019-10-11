12b7467395ae179263ac3a7455a19f274325d9c8
AS3-2-18 <AS3-2-18@uni.lan>
2019-10-11 08:06:58 +0200

    a

--- test.php
+++ test.php
@@ -5,6 +5,7 @@
 
 $v = [1,5,6];
 $suma = 0;
+$suma1 = 0;
 
 //for ($x = 0; $x <3; $x++){
     //echo "The number is : $x <br>";
@@ -14,10 +15,20 @@ $suma = 0;
 //}
 
 $x=0;
-while($x < 3) {
-    $suma = $suma + $v[$x];
-    $x++;
+//while($x < 3) {
+    //$suma = $suma + $v[$x];
+    //$x++;
     
+//}
+//echo "batura : " . $suma;
+
+
+
+//foreach ($v as $suma) {
+    //$suma1 = $suma1 + $suma;
+    //echo $suma1;
 }
-echo "batura : " . $suma;
+for ($x = 0; $x <3; $x++){
+
+
 ?>
