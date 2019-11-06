8eba6f1964659cff6e116ff84dee32a037a484dd
AS3-2-18 <AS3-2-18@uni.lan>
2019-11-06 12:30:23 +0100

    a

---
+++ OOP/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+function echo_izena(){
+echo "kaixo" . "<br>";
+
+}
+echo_izena();
+
+
+?> 
--- array-basico.php
+++ array-basico.php
@@ -1,51 +1,66 @@
 <?php
-$x = [1,5,6,99,12,11,111];
-//echo $suma = $v[0] + $v[1];
+echo "<h3> 1 Dimentsioko array-ak </h3>";
+$v = [1,5,6,5,77,88,333,444,66,0];
+//$suma = $v[0] + $v[1] + $v[2];
+//echo "batura : " . $suma;
 // for
-//$suma = 0;
-//echo "suma: " . $suma . "<br>";
-//echo "for....<br>";
-//for ($i=0; $i < count($v); $i++) {
-    //$suma = $suma + $v[$i];
-    //echo "i: " . $i . ";  ";
-    //echo "v[$i]: " . $v[$i] . "; ";
-    //echo "suma: " . $suma . "<br>";    
-//}
-//echo "...for<br>";
-//echo "$suma <br><br>";
- 
-// foreach
-//$suma = 0;
-//foreach ($v as $x) {
-    //echo "x: $x; suma: $suma <br>";
-    //$suma = $suma + $x;
-//}
-//echo "Suma con foreach: $suma <br>";
-// Dos dimensiones
-//$w = 
-//[
-    //[688,1,6,66,221],
-    //[1,-12],
-    //[-4,0,66,666,4,3]    
-//];
-
-//foreach ($w as $v) {
-    //foreach ($v as $x) {
-        //$suma = $suma + $x;
-    //}
-//}
-//echo "Suma de array de dos dim: $suma <br><br>";
-// for 2 dim
-//$suma = 0;
-//for ($i=0; $i < count($w); $i++) {
-    //for ($j=0; $j < count($w[$i]); $j++) {
-        //$suma = $suma + $w[$i][$j];
-    //}
-//}
-//echo "Suma 2 dim con for: $suma";
-
-foreach {
-
+$suma = 0;
+for ($i = 0; $i < count($v); $i++) {
+    //echo "The number is: $x <br>";
+    //echo " i:" . $i;
+    $suma = $suma + $v[$i];
+    //echo "; suma:" . $suma;
+    //echo "<br>";
 }
-
-?> 
+echo "batura for: " . $suma . "<br>";
+// while
+$suma = 0;
+$i = 0;
+while ($i < count($v)) {
+    $suma = $suma + $v[$i];
+    $i++;
+}
+echo "batura while: " . $suma . "<br>";
+// foreach
+foreach ($v as $x) {
+    //echo $x . "<br>";
+    $suma = $suma + $x;
+}
+echo "batura foreach: " . $suma;
+/************************
+2 dimentsiotako arrayak
+*/
+echo "<h3> 2 Dimentsioko array-ak </h3>";
+$vv = [[1,2,3],[4,5,6]];
+// for
+$suma = 0;
+for ($i=0; $i<count($vv); $i++) {
+    for ($j=0; $j<count($vv[0]); $j++) {
+        //echo $vv[$i][$j];
+        $suma = $suma + $vv[$i][$j];
+    }
+}
+echo "batura for: " . $suma . "<br>";
+// while
+$suma = 0;
+$i=0;
+$j=0;
+while ($i<count($vv)) {
+    $j=0;
+    while ($j<count($vv[0])) {
+        //echo $vv[$i][$j];
+        $suma = $suma + $vv[$i][$j];
+        $j++;
+    }
+    $i++;
+}
+echo "batura while: " . $suma . "<br>";
+// foreach
+$suma = 0;
+foreach ($vv as $v) {
+    foreach($v as $x) {
+        $suma = $suma + $x;
+    }
+}
+echo "batura foreach: " . $suma;
+?>
