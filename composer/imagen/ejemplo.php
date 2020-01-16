d00feb4a8b48a41250a9b2b54de63a525152825c
javiersaez2 <asaez22.az@gmail.com>
2020-01-16 01:09:53 -0800

    :)

--- composer/imagen/ejemplo.php
+++ composer/imagen/ejemplo.php
@@ -1,22 +1,17 @@
-
-<?php
+<?php  
 require 'vendor/autoload.php';
 
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
 $tr = new GoogleTranslate('en'); // Translates into English 
-
-?>
-
+echo $tr->translate( $_GET['nombre']);?>
 <!DOCTYPE html>
 <html>
 <body>
 1
-<form action="ejemplo.php" method="get">
-  <p>Nombre: <input type="text" name="nombre" size="40"></p>
+<form action="traductor.php" method="get">
+  <p>Nombre: <input type="text" name="nombre" value="<?php echo $_GET['nombre'] ?>" size="40"></p>
 
-    <input type="submit" value="Enviar">
-    <input type="reset" value="Borrar">
   </p>
 </form>
 
