cc672eadd9aa8550a9e67e94733140f394e414f1
javiersaez2 <asaez22.az@gmail.com>
2020-01-22 04:21:50 -0800

    PUEDE

--- composer/imagen/ejemplo.php
+++ composer/imagen/ejemplo.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="login.css">
 
 <?php
-$servername = "192.168.71.10:3306";
+$servername = "localhost";
 $username = "javis";
 $password = "Admin123";
 $dbname = "asir2_crud";
@@ -49,40 +49,115 @@ if ($result->num_rows > 0) {
 }
 $conn->close();
 
-$tr = new GoogleTranslate(); // Translates into English 
+$tr = new GoogleTranslate(en); // Translates into English 
 ?>
 
 
 <!DOCTYPE html>
 <html>
+<style>
+
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+<style type="text/css">
+.form-style-8{
+	font-family: 'Open Sans Condensed', arial, sans;
+	width: 500px;
+	padding: 30px;
+	background: #FFFFFF;
+	margin: 50px auto;
+	box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+	-moz-box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+	-webkit-box-shadow:  0px 0px 15px rgba(0, 0, 0, 0.22);
+
+}
+.form-style-8 h2{
+	background: #4D4D4D;
+	text-transform: uppercase;
+	font-family: 'Open Sans Condensed', sans-serif;
+	color: #797979;
+	font-size: 18px;
+	font-weight: 100;
+	padding: 20px;
+	margin: -30px -30px 30px -30px;
+}
+.form-style-8 input[type="text"],
+.form-style-8 input[type="date"],
+.form-style-8 input[type="datetime"],
+.form-style-8 input[type="email"],
+.form-style-8 input[type="number"],
+.form-style-8 input[type="search"],
+.form-style-8 input[type="time"],
+.form-style-8 input[type="url"],
+.form-style-8 input[type="password"],
+.form-style-8 textarea,
+.form-style-8 select 
+{
+	box-sizing: border-box;
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box;
+	outline: none;
+	display: block;
+	width: 100%;
+	padding: 7px;
+	border: none;
+	border-bottom: 1px solid #ddd;
+	background: transparent;
+	margin-bottom: 10px;
+	font: 16px Arial, Helvetica, sans-serif;
+	height: 45px;
+}
+.form-style-8 textarea{
+	resize:none;
+	overflow: hidden;
+}
+.form-style-8 input[type="button"], 
+.form-style-8 input[type="submit"]{
+	-moz-box-shadow: inset 0px 1px 0px 0px #45D6D6;
+	-webkit-box-shadow: inset 0px 1px 0px 0px #45D6D6;
+	box-shadow: inset 0px 1px 0px 0px #45D6D6;
+	background-color: #2CBBBB;
+	border: 1px solid #27A0A0;
+	display: inline-block;
+	cursor: pointer;
+	color: #FFFFFF;
+	font-family: 'Open Sans Condensed', sans-serif;
+	font-size: 14px;
+	padding: 8px 18px;
+	text-decoration: none;
+	text-transform: uppercase;
+}
+.form-style-8 input[type="button"]:hover, 
+.form-style-8 input[type="submit"]:hover {
+	background:linear-gradient(to bottom, #34CACA 5%, #30C9C9 100%);
+	background-color:#34CACA;
+}
+</style>
 <body>
 
 <form action="ejemplo.php" method="get">
+<div class="form-style-8">
+tra<textarea placeholder="Message" onkeyup="adjust_textarea(this)" name="nombre2" value="<?php echo $_GET['nombre2'] ?>"></textarea>
+</div>
+
   <p>Traducir: <input type="text" name="nombre" value="<?php echo $_GET['nombre'] ?>" size="40"></p>
   <input type="submit" value="Enviar">
     <input type="reset" value="Borrar">
 
   </p>
 </form>
-Traducido: <?php echo $tr->translate( $_GET['nombre']);?>
+Traducido: <?php echo $tr->translate( $_GET['nombre2']);?>
 
 
 
-
-
-
-<?php //Ejemplo funciones básicas aprenderaprogramar.com
-$uno = 'Diego';
-$dos = 'eeee';
-$texto = "Donde dije digo digo Diego.";
-
-echo str_replace($uno, $dos, $texto);
-
-echo "<br />";
-
-echo $texto;
-
-?>
 <br>
 </body>
+
+
+<script type="text/javascript">
+    //auto expand textarea
+    function adjust_textarea(h) {
+        h.style.height = "20px";
+        h.style.height = (h.scrollHeight)+"px";
+    }
+    </script>
 </html>
--- composer/imagen/pruebas.php
+++ composer/imagen/pruebas.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="login.css">
 
 <?php
-$servername = "192.168.71.10:3306";
+$servername = "localhost";
 $username = "javis";
 $password = "Admin123";
 $dbname = "asir2_crud";
@@ -14,30 +14,7 @@ if ($conn->connect_error) {
 
 
 
-if(isset($_POST['save'])){
-  $sql= ("UPDATE idiomas SET letra ='$_POST[nuevo]' 
-  WHERE letra='$_POST[viejo2]'");
-  
-  $result = $conn->query($sql);
-  header("Location: pruebas.php");
-  }
-  
-  ?>
-    <div class="login-page">
-      <div class="form">
-      
-  <form method="post"> 
-  Producto a cambiar</br>
-  <input type="text" name="viejo2"><br/>
-  
-  Precio a cambiar</br>
-  <input type="text" name="nuevo"><br/>
-  
-  
-  
-  <button type="submit" name="save">save</button>
-
-
+?>
 
 </body>
 </html>
